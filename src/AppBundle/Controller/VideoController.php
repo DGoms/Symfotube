67419ad3dd0fd2561373536d75a2d15ec0461d53
goms <dgomesh@gmail.com>
2018-01-10 17:07:22 +0100

    video home + video show ok

--- src/AppBundle/Controller/VideoController.php
+++ src/AppBundle/Controller/VideoController.php
@@ -13,10 +13,22 @@ class VideoController extends Controller
     public function indexAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
-        $videos = $em->getRepository('AppBundle:Video')->findBy(array(), array('datetime' => 'desc'));
+        $videos = $em->getRepository('AppBundle:Video')->findBy(array(), array('datetime' => 'asc'));
         
         return $this->render('AppBundle::video/index.html.twig', [
             'videos' => $videos
+        ]);
+    }
+    
+    public function showAction(Request $request, int $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $video = $em->getRepository('AppBundle:Video')->find($id, array('datetime' => 'asc'));
+        $comments = $em->getRepository('AppBundle:Comment')->findByVideo($video, array('datetime' => 'asc'));
+        
+        return $this->render('AppBundle::video/show.html.twig', [
+            'video' => $video,
+            'comments' => $comments
         ]);
     }
 }
